6ae30381d5b813ace7c34599e68527c417691e9b
Yotam Doron <ydoron@google.com>
2019-07-05 11:51:39 +0100

    Updates
    PiperOrigin-RevId: 256650517
    Change-Id: If235584caf102f606dcd181f172114dc29c9de1a

--- bsuite/kokoro/test.sh
+++ bsuite/kokoro/test.sh
@@ -4,17 +4,17 @@ set -x
 sudo apt-get install python3-pip
 sudo pip3 install virtualenv
 
-virtualenv -p /usr/bin/python3.6 ~/bsuite/venv
-source ~/bsuite/venv/bin/activate
+virtualenv -p /usr/bin/python3.6 bsuiteenv
+source bsuiteenv/bin/activate
 
-git clone sso://team/dm-env-owners/dm_env ~/bsuite/temp/dm_env
-pip3 install ~/bsuite/temp/dm_env
+git clone sso://team/dm-env-owners/dm_env
+pip3 install dm_env/
 
-git clone sso://team/deepmind-eng/bsuite ~/bsuite/temp/bsuite
-pip3 install ~/bsuite/temp/bsuite
+git clone sso://team/deepmind-eng/bsuite
+pip3 install bsuite/
 
 pip3 install nose
-nosetests tests/environments_test.py
+nosetests bsuite/bsuite/tests/environments_test.py
 
 python3 -c "import bsuite
 env = bsuite.load_from_id('catch/0')
@@ -22,4 +22,6 @@ env.reset()"
 
 
 deactivate
-rm -rf ~/bsuite/temp
+rm -rf bsuiteenv/
+rm -rf bsuite/
+rm -rf dm_env/
