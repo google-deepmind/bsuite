61d02a899cf5da6c4dae8ae1f8521f2cae03b6d0
Yotam Doron <ydoron@google.com>
2019-07-10 15:30:07 +0100

    Updates
    PiperOrigin-RevId: 257400435
    Change-Id: I9d6967c350be0bbf50260afb3981cf5cdc38ff20

--- bsuite/utils/timer.py
+++ bsuite/utils/timer.py
@@ -23,7 +23,7 @@ from __future__ import print_function
 import functools
 import time
 import pandas as pd
-from typing import List, Tuple, Text
+from typing import Any, List, Tuple, Text
 
 
 def time_run(func):
