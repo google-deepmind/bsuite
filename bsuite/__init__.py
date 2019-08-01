5bb4b87f4297ac4089e7418a3d777182a8eae261
Ian Osband <iosband@google.com>
2019-08-01 07:16:35 -0700

    Surface logging options to package level.
    PiperOrigin-RevId: 261116665
    Change-Id: Iabce1bfbad4abe14fcde9e054da8582316c0b105

--- bsuite/__init__.py
+++ bsuite/__init__.py
@@ -38,3 +38,5 @@ from . import bsuite as _bsuite
 
 load = _bsuite.load
 load_from_id = _bsuite.load_from_id
+load_and_record_to_sqlite = _bsuite.load_and_record_to_sqlite
+load_and_record_to_csv = _bsuite.load_and_record_to_csv
