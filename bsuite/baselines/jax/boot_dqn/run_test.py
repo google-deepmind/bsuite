708c1c57fd4ab6069ac992b22473cb0d9028e798
Thomas Köppe <tkoeppe@google.com>
2020-04-16 09:52:01 -0700

    Reduce parameter values to speed up the unit test.
    PiperOrigin-RevId: 306865866
    Change-Id: I49d736307023afd5b3403212c9de980a9023a0eb

--- bsuite/baselines/jax/boot_dqn/run_test.py
+++ bsuite/baselines/jax/boot_dqn/run_test.py
@@ -31,7 +31,8 @@ class RunTest(parameterized.TestCase):
   @classmethod
   def setUpClass(cls):
     super().setUpClass()
-    FLAGS.num_episodes = 5
+    FLAGS.num_ensemble = 8
+    FLAGS.num_episodes = 2
     FLAGS.logging_mode = 'terminal'
 
   @parameterized.parameters(*sweep.TESTING)
